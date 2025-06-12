7868e79144b2344239029076eb0adbbd0a8ffc2e
MontehBiglari <156302023+MontehBiglari@users.noreply.github.com>
2025-06-11 23:08:02 -0700

    Create Lion.java

---
+++ Lion.java
@@ -0,0 +1,10 @@
+public class Lion extends Animal {
+  private double maneLength;
+
+  public Lion(String name, String species, int age, double maneLength) {
+    super(name, species, age);
+    this.maneLength = maneLength;
+  }
+
+  @override 
+}
