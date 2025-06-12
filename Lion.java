001baa05bca219f6e494e70e90f74d8b2d392578
MontehBiglari <156302023+MontehBiglari@users.noreply.github.com>
2025-06-11 23:24:29 -0700

    Update Lion.java

--- Lion.java
+++ Lion.java
@@ -6,5 +6,11 @@ public class Lion extends Animal {
     this.maneLength = maneLength;
   }
 
-  @override 
+  @Override
+  String GetDetails() {
+    String details = String.format("Name: %s\n", this.name);
+    details += String.format("Species: %s\n", this.species);
+    details += String.format("Age: %d\n", this.age);
+    details += String.format("Mane Length: %f", this.maneLength);
+  }
 }
