4b8a21edab87e2ee394fbf19b7470e4218f0892b
MontehBiglari <156302023+MontehBiglari@users.noreply.github.com>
2025-06-11 22:50:54 -0700

    Create Animal.java

---
+++ Animal.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Animal {
+    private String name;
+    private String species;
+    private int age;
+
+    public Animal(String name, String species, int age) {
+      this.name = name;
+      this.species = species;
+      this.age = age;
+    }
+
+    public String GetName() {
+      return name;
+    }
+
+    public String GetSpecies() {
+      return species;
+    }
+
+    public int GetAge() {
+      return age;
+    }
+
+    public abstract String GetDetails() {
+      
+    }
+    
