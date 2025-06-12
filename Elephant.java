01fd45547454c2bd4e4d5086f75cd48b5baa4a1e
MontehBiglari <156302023+MontehBiglari@users.noreply.github.com>
2025-06-11 23:30:14 -0700

    Create Elephant.java

---
+++ Elephant.java
@@ -0,0 +1,17 @@
+public class Elephant extends Animal {
+    private double trunkLength;
+
+    public Lion(String name, String species, int age, double trunkLength) {
+        super(name, species, age);
+        this.trunkLength = trunkLength;
+    }
+
+    @Override
+    public String getDetails() {
+        String details = String.format("Name: %s%n", GetName());
+        details += String.format("Species: %s%n", GetSpecies());
+        details += String.format("Age: %d%n", GetAge());
+        details += String.format("Trunk Length: %f", trunkLength);
+        return details;
+    }
+}
