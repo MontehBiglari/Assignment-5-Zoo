334292caa65abef9d0afdc34b55c3a478d83cc7f
MontehBiglari <156302023+MontehBiglari@users.noreply.github.com>
2025-06-11 23:32:06 -0700

    Create Penguin.java

---
+++ Penguin.java
@@ -0,0 +1,17 @@
+public class Penguin extends Animal {
+    private double swimmingSpeed;
+
+    public Lion(String name, String species, int age, double swimmingSpeed) {
+        super(name, species, age);
+        this.swimmingSpeed = swimmingSpeed;
+    }
+
+    @Override
+    public String getDetails() {
+        String details = String.format("Name: %s%n", GetName());
+        details += String.format("Species: %s%n", GetSpecies());
+        details += String.format("Age: %d%n", GetAge());
+        details += String.format("Swimming Speed: %f km/h", swimmingSpeed);
+        return details;
+    }
+}
