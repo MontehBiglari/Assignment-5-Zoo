fc343ae9455419c17a01bba42b2cccd5e549ca39
MontehBiglari <156302023+MontehBiglari@users.noreply.github.com>
2025-06-11 23:28:04 -0700

    Update Lion.java

--- Lion.java
+++ Lion.java
@@ -1,16 +1,17 @@
 public class Lion extends Animal {
-  private double maneLength;
+    private double maneLength;
 
-  public Lion(String name, String species, int age, double maneLength) {
-    super(name, species, age);
-    this.maneLength = maneLength;
-  }
+    public Lion(String name, String species, int age, double maneLength) {
+        super(name, species, age);
+        this.maneLength = maneLength;
+    }
 
-  @Override
-  String GetDetails() {
-    String details = String.format("Name: %s\n", this.name);
-    details += String.format("Species: %s\n", this.species);
-    details += String.format("Age: %d\n", this.age);
-    details += String.format("Mane Length: %f", this.maneLength);
-  }
+    @Override
+    public String getDetails() {
+        String details = String.format("Name: %s%n", GetName());
+        details += String.format("Species: %s%n", GetSpecies());
+        details += String.format("Age: %d%n", GetAge());
+        details += String.format("Mane Length: %f", maneLength);
+        return details;
+    }
 }
